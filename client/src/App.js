a029046e60b05048044fe2400b50cb64c95e4597
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-10-10 23:03:30 -0700

    building app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Route } from "react-router-dom";
-import SavedList from "./Movies/SavedList";
-import MovieList from "./Movies/MovieList";
-import Movie from "./Movies/Movie";
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { Route } from 'react-router-dom'
+import SavedList from './Movies/SavedList'
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
+import axios from 'axios'
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
